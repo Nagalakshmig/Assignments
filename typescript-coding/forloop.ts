75af89b9b58cae6edcb138567f88bad415edf719
Nagalakshmig <gnagu19@gmail.com>
2026-08-05 00:39:03 +0530

    assignment5.ts

---
+++ typescript-coding/forloop.ts
@@ -0,0 +1,14 @@
+
+// printing even number with for loop
+
+for (let i:number=1 ; i<=10 ;i++)
+{
+    if(i%2==0){
+        console.log( +i+" is even number ");
+            }
+            else
+            {
+             console.log( +i+" is odd number ");   
+            }
+}
+
